0a5184c9887a043c5a5dce1586314c3a171614d9
LockedFog <AboxYXY@outlook.com>
2022-05-31 21:26:24 +0800

    may 31

--- PowerCommand.cpp
+++ PowerCommand.cpp
@@ -16,11 +16,11 @@ template<class t>inline void fw(t num,char ch){
     if(num<0)num=-num,putchar('-');
     fw(num);putchar(ch);
 }
-
-
-
+int user_num;
+map<string,string>Pwd;
 inline void logout(string s,string type,string from){
-    freopen("logs.out","w",stdout);
+    ofstream output;
+    output.open("logs.out",ios::app);
     time_t now_time=time(NULL);
     tm* t_tm=localtime(&now_time);
     string time_now=asctime(t_tm);
@@ -28,15 +28,42 @@ inline void logout(string s,string type,string from){
     GO(0,time_now.length()-2,i)ch[i]=time_now[i];
     string chh=ch;
     string out="["+chh+"]["+type+"]["+from+"]"+s+"\n";
-    cout<<out;
+    output<<out;
+    output.close();
+}
+inline void logclear(){
+    freopen("logs.out","w",stdout);
+    cout<<"Cleared.\n";
     fclose(stdout);
+}
+
+inline void read_set(){
+    logout("开始读取settings.in","INFO","read_set()");
+    user_num=fr<int>();
+    GO(1,user_num,i){
+        string userName,userPwd;
+        cin>>userName>>userPwd;
+        Pwd[userName]=userPwd;
+    }
+    if(user_num==0){
+        logout("没有用户处于用户列表中!","WARN","read_set()");
+    }else{
+        char ch[10001];
+        sprintf(ch,"读取到%d名用户",user_num);
+        string logs=ch;
+        logout(logs,"INFO","read_set()");
+    }
+    logout("完成读取settings.in","INFO","read_set()");
 }
 
 
 signed main(){
+    logclear();
+    logout("程序启动成功!","INFO","main()");
     freopen("settings.in","r",stdin);
     logout("打开settings.in","INFO","main()");
-
-
+    read_set();
+    fclose(stdin);
+    logout("关闭settings.in","INFO","main()");
     return 0;
 }
